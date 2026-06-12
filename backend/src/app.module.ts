951302341294b4e173717a6d3e75ac2f21ece2f4
Prestige Nsien <prestigensien45@gmail.com>
2026-06-12 10:50:01 +0100

    feat(modules): integrate SchedulerModule into AppModule and JobsModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './config/database.config';
 import { JobsModule } from './modules/jobs/jobs.module';
+import { SchedulerModule } from './modules/workers/scheduler/scheduler.module';
+import { WorkerModule } from './modules/workers/workers.module';
 
 @Module({
-  imports: [DatabaseModule, JobsModule],
+  imports: [DatabaseModule, JobsModule, SchedulerModule, WorkerModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/modules/jobs/jobs.module.ts
+++ backend/src/modules/jobs/jobs.module.ts
@@ -4,9 +4,13 @@ import { JobsService } from "./jobs.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Job } from "./entities/job.entity";
 import { JobModelAction } from "./actions/job.action";
+import { SchedulerModule } from "../workers/scheduler/scheduler.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Job])],
+  imports: [
+    TypeOrmModule.forFeature([Job]),
+    SchedulerModule,
+],
   controllers: [JobsController],
   providers: [JobsService, JobModelAction],
 })
